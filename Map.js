34f52019013dccf1b109bb6dd4f04c3d7b9ef98c
Nicholas Tesija <ntesija@umich.edu>
2017-02-11 13:34:31 -0500

    only one marker active, sorted list

--- Map.js
+++ Map.js
@@ -1,59 +1,56 @@
 var globalMap;
+var marker;
 
 $(function() {
-
-var MapFcns = {
-    loadSiteList: function () {
-        var airportList = $('#airport-list');
-            airportList.html('');
-            airportList.append('<option value=""></option>');
-        for (var i in sites) {
-            var newOption = $('<option value="' + sites[i].Code + '">' + sites[i].Code + '</option>');
-            airportList.append(newOption);
-        }
-    },
-    
-    siteListChange: function() {
-        var ctl = $(this),
-            airportCode = ctl.val();
-            if(airportCode) {
-                var currAirport = _.findWhere(sites, {Code: airportCode});
-                $('#setting-code').text(currAirport.Code);
-                $('#setting-city').text(currAirport.City);
-                
-                var marker = new google.maps.Marker({
-                    position: {lat: currAirport.Latitude, lng: currAirport.Longitude},
-                    map: globalMap,
-                    title: currAirport.Code
-                });
-            }
-    }
-}
-
-
-MapFcns.loadSiteList();
-$('#airport-list').change(MapFcns.siteListChange);
-$('#exercise-toggle').click(function() {
-    var  toggleCtl = $(this),
-         toggleVal = toggleCtl.text();
-    if (toggleVal == '-') {
-        toggleCtl.text('+');
-        $('#exercise-instructions').hide();
-    } else {
-        toggleCtl.text('-');
-        $('#exercise-instructions').show();
-    }
+	var MapFcns = {
+		loadSiteList: function () {
+			var airportList = $('#airport-list');
+				airportList.html('');
+				airportList.append('<option value=""></option>');
+				
+			sites.sort( function(a, b) {
+				return a.Code.localeCompare(b.Code);
+			});
+			for (var i in sites) {
+				var newOption = $('<option value="' + sites[i].Code + '">' + sites[i].Code + '</option>');
+				airportList.append(newOption);
+			}
+		},
+		siteListChange: function() {
+			var ctl = $(this),
+			airportCode = ctl.val();
+			if(airportCode) {
+				var currAirport = _.findWhere(sites, {Code: airportCode});
+				$('#setting-code').text(currAirport.Code);
+				$('#setting-city').text(currAirport.City);
+				
+				if (marker) {
+					marker.setMap(null);
+				}
+				marker = new google.maps.Marker({
+					position: {lat: currAirport.Latitude, lng: currAirport.Longitude},
+					map: globalMap,
+					title: currAirport.Code
+				});
+			}
+		}
+	}
+	
+	MapFcns.loadSiteList();
+	$('#airport-list').change(MapFcns.siteListChange);
+	$('#exercise-toggle').click(function() {
+		var  toggleCtl = $(this),
+			 toggleVal = toggleCtl.text();
+		if (toggleVal == '-') {
+			toggleCtl.text('+');
+			$('#exercise-instructions').hide();
+		} else {
+			toggleCtl.text('-');
+			$('#exercise-instructions').show();
+		}
+	});
 });
-
-});
-
-
-
-
-
-
-
-    
+   
 function  initMap() {
   // Callback function to create a map object and specify the DOM element for display.
   globalMap = new google.maps.Map(document.getElementById('airport-map'), {
@@ -61,5 +58,4 @@ function  initMap() {
     scrollwheel: true,
     zoom: 6
   });
-  
-    }
+}
